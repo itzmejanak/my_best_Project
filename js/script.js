15410225aa92da3685997b6ecb163c1ddec59398
itzmejanak <itzmejanak@gmail.com>
2023-10-08 16:09:25 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/**========================================typyng animation===================================**/
+
+
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Software Developer", "Coder"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
