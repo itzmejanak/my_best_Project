4f742d496f9074e4c94b7c6a2767c19641ea4a6d
Janak Devkota <itzmejanak@gmail.com>
2024-02-16 22:16:04 +0545

    🎉 init:

---
+++ images/portfolio/07_Projects/08_Project/script.js
@@ -0,0 +1,42 @@
+let btn = document.querySelector("#search-btn");
+const api = "46440f5961e110bd4a5a8ec7b377caf1";
+
+async function checkWeather() {
+    let input = document.querySelector("#input").value;
+    let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${api}`);
+    var finalData = await data.json();
+    console.log(finalData);
+    document.querySelector("#cityName").innerHTML = finalData.name;
+    document.querySelector("#celcius").innerHTML = Math.round(finalData.main.temp) + " °F";
+    document.querySelector("#hum").innerHTML = finalData.main.humidity + "%";
+    document.querySelector("#speed").innerHTML = finalData.wind.speed + " Km/h";
+
+    let img = document.querySelector("#middle img");
+    switch (finalData.weather[0].main) {
+        case "Clear":
+            img.src = "/images/clear.png";
+            break;
+        case "Clouds":
+            img.src = "/images/clouds.png";
+            break;
+        case "Drizzle":
+            img.src = "/images/drizzle.png";
+            break;
+        case "Mist":
+            img.src = "/images/mist.png";
+            break;
+        default:
+            img.src = "/images/rain.png";
+            break;
+    }
+}
+
+btn.addEventListener("click", () => {
+    if(document.querySelector("#input").value.trim() == ""){
+        document.querySelector("#cityName").innerHTML = "Invalid Search"
+    }else{
+        document.querySelector("#down").style.display = "block"
+        checkWeather();
+    }
+   
+});
