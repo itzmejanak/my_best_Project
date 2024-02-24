91ce0223d9d8400894813f9c8758a84aae25adc6
Janak Devkota <itzmejanak@gmail.com>
2024-02-24 15:18:11 +0545

    🎉 init:

--- images/portfolio/07_Projects/More/10_Project/script.js
+++ images/portfolio/07_Projects/More/10_Project/script.js
@@ -1,20 +1,38 @@
-function appendToResult(value) {
-    let result = document.getElementById("result");
-    let currentResult = result.textContent;
-    result.textContent = currentResult + value;
+document.addEventListener("DOMContentLoaded", function() {
+    // Selecting elements
+    const resultTextarea = document.getElementById("result");
+    const resultDiv = document.getElementById("result1");
+    const buttons = document.querySelectorAll(".btn");
+    const clearButton = document.getElementById("clear");
 
-    if (value === "=") {
-        result.textContent = eval(currentResult);
-            
-        }
-    else if (value === 'C') {
-            result.innerHTML = "0";
-        }
-    else if (value === '☒') {
-            result.innerHTML = currentResult.slice(0, -1);
-        }
-    else if(value === 'x²'){
-                result.textContent = Math.pow(parseFloat(currentResult), 2)
-        }
-}
-
+    // Adding click event listeners to buttons
+    buttons.forEach(button => {
+        button.addEventListener("click", function() {
+            // console.log(button);
+            if(button.textContent === "C"){
+                resultTextarea.value = ""
+                resultDiv.innerHTML = ""
+            }else if(button.textContent === "☒"){
+                let res = resultTextarea.value;
+                resultTextarea.value = res.slice(0, -1);
+            }else if(button.textContent === "="){
+                const result = eval(resultTextarea.value);
+                resultTextarea.value = result;
+                resultDiv.textContent = result;
+            }else if(button.textContent === "x²"){
+                const result = eval(resultTextarea.value);
+                resultTextarea.value = Math.pow(Number(result), 2);
+                resultDiv.textContent = Math.pow(parseFloat(result), 2);
+            }else if(button.textContent === "x"){
+                resultTextarea.value += "*";
+            }
+            else{
+                if(resultTextarea.value === "0"){
+                    resultTextarea.value = button.textContent;
+                }else{
+                    resultTextarea.value += button.textContent
+                }
+            }
+        });
+    });
+});
